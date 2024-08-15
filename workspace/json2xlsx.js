b2768e425359c3a6e941a70760f3a4eca7c1aa19
Alexandr <aleksandr.m@devs.ae>
2024-08-15 16:15:13 +0400

    refactor: constants.js file moved to /workspace folder

--- workspace/json2xlsx.js
+++ workspace/json2xlsx.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const { json2xlsx } = require('../index');
-const { GENERATION_FOLDER_NAME } = require('../constants');
+const { GENERATION_FOLDER_NAME } = require('./constants');
 
 json2xlsx({
   inputPath: path.resolve(__dirname, '../locales'),
--- workspace/xlsx2json.js
+++ workspace/xlsx2json.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const { xlsx2json } = require('../index');
-const { GENERATION_FOLDER_NAME } = require('../constants');
+const { GENERATION_FOLDER_NAME } = require('./constants');
 
 xlsx2json({
   inputPath: path.resolve(__dirname, `../${GENERATION_FOLDER_NAME}/locales.xlsx`),
